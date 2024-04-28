a1cf5c6c3dc681501222622564f699e89b89c43e
MiguelFullStack <miguel.fullstack@gmail.com>
2024-04-27 23:57:28 -0400

    vercel new

--- db/connectdb.mjs
+++ db/connectdb.mjs
@@ -5,9 +5,9 @@ export const connectdb = async() => {
         
         // await mongoose.connect( 'mongodb+srv://compa:hola1234$@cluster0.6rpwwim.mongodb.net/huggo-scam' );
         // await mongoose.connect( 'mongodb+srv://MiguelFullstack:hookom119@hook.lb2oqov.mongodb.net/?retryWrites=true&w=majority' );
-        // await mongoose.connect( 'mongodb+srv://MiguelFullstack:hookom119@miguelfullstack.xbouv6j.mongodb.net/soler-DC' );
+        await mongoose.connect( 'mongodb+srv://MiguelFullstack:hookom119@hook.lb2oqov.mongodb.net/test' );
         // await mongoose.connect( 'mongodb+srv://MiguelFullstack:hookom119@miguelfullstack.xbouv6j.mongodb.net/pablo-escobar' );
-        await mongoose.connect('mongodb://127.0.0.1:27017/huggo-dev')
+        // await mongoose.connect('mongodb://127.0.0.1:27017/huggo-dev')
         return console.log('Base de datos online');
 
     } catch (error) {

6cbd2ef1b1785f9cd5e67d92dba484ccb5486fa9
MiguelFullStack <miguel.fullstack@gmail.com>
2024-09-09 11:28:50 -0400

    salvacion

--- index.mjs
+++ index.mjs
@@ -34,9 +34,6 @@ const __dirname = path.dirname(__filename)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
 app.use('/auth', authRoute )
 
 
